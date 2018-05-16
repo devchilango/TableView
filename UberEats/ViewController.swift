c609d78690d33382914ad4f0209886dce706d876
lalo <guerreroglez@gmail.com>
2018-05-15 19:03:03 -0500

    carga inicial

--- UberEats/ViewController.swift
+++ UberEats/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let cellIdentifier = "cell"
+    var mainTable:UITableView!
+    var rows = 5
+    let sizeCells:CGFloat = 300
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainTable = UITableView(frame: UIScreen.main.bounds)
+        mainTable.delegate = self
+        mainTable.dataSource = self
+        mainTable.register( UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        
+        self.view.addSubview(mainTable)
+        self.view.backgroundColor = .white
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rows
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return sizeCells
+    }
+    
+    
 }
 
